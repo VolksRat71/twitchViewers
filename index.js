de0bff92a3384c75c6fcae2af293e2bc6d267ed0
VolksRat71 <nathanieljryan1994@gmail.com>
2022-07-30 18:23:03 -0500

    getting rid of unneeded command

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const chromeProfileList = require("chrome-profile-list");
 const endPoint = process.env.ENDPOINT;
 const cliOpeningProcess = process.env.CLIOPENINGPROCESS;
 
-exec('"/path/to/test file/test.sh" arg1 arg2');
 chromeProfileList().forEach(({ profileDirName }, i) => {
   setTimeout(() => {
     const command = `${cliOpeningProcess} ${endPoint} --args --profile-directory="${profileDirName}"`;
